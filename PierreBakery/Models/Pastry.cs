3d6afd52de2196e0c02bc79c57fc5a4369434780
James Fox <jimmyfox00@gmail.com>
2022-05-06 13:08:49 -0700

    Add Basic UI

--- PierreBakery/Models/Pastry.cs
+++ PierreBakery/Models/Pastry.cs
@@ -6,6 +6,5 @@ namespace PastryItem
     public override int Cost { get; set; }
     public override int DiscountRate { get; set; }
     public Pastry(int count, int cost, int discountRate) : base(count, cost, discountRate){}
- 
   }
 }
--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using BreadLoaf;
+using PastryItem;
 
 public class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Welcome to Pierre's Bakery");
+    Console.WriteLine("Today we are serving Bread and Pastries");
+    Console.WriteLine("Bread is $5 per loaf, but if you buy 2, the third is free");
+    Console.WriteLine("A pastry is $2, but if you buy 2, the third is half off");
+    Console.WriteLine("How many loaves of bread would you like to buy?");
+    int breadCount = int.Parse(Console.ReadLine());
+    Bread bread = new Bread(breadCount, 5, 0);
+    Console.WriteLine("How many pastries would you like to buy?");
+    int pastryCount = int.Parse(Console.ReadLine());
+    Pastry pastry = new Pastry(pastryCount, 2, 1);
+    Console.WriteLine("Your total is: " + (bread.CalculateCost() + pastry.CalculateCost()));
   }
 }
